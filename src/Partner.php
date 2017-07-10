60285c05a21b781de8d87b5c42d0dd7851cd4784
Ramin Farmani <ramin.farmani@eigital.com>
2017-07-10 17:08:47 +0430

    add partner module

---
+++ src/Partner.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ramin
+ * Date: 6/7/2017
+ * Time: 5:55 PM
+ */
+
+namespace eigitallabs\ePaisa;
+
+
+/**
+ * Class Profile
+ * @package eigitallabs\ePaisa
+ */
+class Partner
+{
+    /**
+     * @var Request
+     */
+    protected $client;
+
+    /**
+     * Profile constructor.
+     * @param Request $client
+     */
+    public function __construct(Request $client)
+    {
+        $this->client = $client;
+        Log::debug("Partner object initiated.");
+    }
+
+    /**
+     * @param array $options
+     * @return array
+     */
+    public function products($options = [])
+    {
+        return $this->client->send('/partner/merchant/product', 'get', $options);
+    }
+
+    /**
+     * @param array $options
+     * @return array
+     */
+    public function merchants($options = [])
+    {
+        return $this->client->send('/partner/merchant', 'get', $options);
+    }
+
+}
